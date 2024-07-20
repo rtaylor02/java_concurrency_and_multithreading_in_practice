754960ebbf5689d378da220a4b92aa6b3f66736f
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2024-07-20 08:30:01 +0100

    Rearrange the files into 1 giant class for compilation and execution simplicity.

--- src/main/java/com/rtaylor02/Main.java
+++ src/main/java/com/rtaylor02/Main.java
@@ -7,46 +7,78 @@ import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        List<Callable<Void>> workers = createWorkers(3, createAppleGarden(6));
-        ForkJoinPool.commonPool().invokeAll(workers);
+        Lesson2_usingForkJoinPoolWithCallables.main();
     }
 
-    private static AppleTree[] createAppleGarden(int numberOfAppleTrees) {
-        AppleTree[] trees = new AppleTree[numberOfAppleTrees];
-
-        for (int i = 0; i < numberOfAppleTrees; i++) {
-            trees[i] = new AppleTree(i, 3);
+    private static class Lesson2_usingForkJoinPoolWithCallables {
+        public static void main(String... args) {
+            List<Callable<Void>> workers = createWorkers(3, createAppleGarden(6));
+            ForkJoinPool.commonPool().invokeAll(workers);
+        }
+    
+        private static AppleTree[] createAppleGarden(int numberOfAppleTrees) {
+            AppleTree[] trees = new AppleTree[numberOfAppleTrees];
+    
+            for (int i = 0; i < numberOfAppleTrees; i++) {
+                trees[i] = new AppleTree(i, 3);
+            }
+    
+            return trees;
+        }
+    
+        private static List<Callable<Void>> createWorkers(int numberOfWorkers, AppleTree[] garden) {
+            String[] names = createWorkerNames(numberOfWorkers);
+    
+            List<Callable<Void>> workers = new ArrayList<>(numberOfWorkers);
+            workers.add(createWorker(0, 2, garden, names[0]));
+            workers.add(createWorker(2, 4, garden, names[1]));
+            workers.add(createWorker(4, 6, garden, names[2]));
+            
+            return workers;
+        }
+    
+        private static String[] createWorkerNames(int numberOfNames) {
+            String[] workerNames = new String[numberOfNames];
+            for (int i = 0; i < numberOfNames; i++) {
+                workerNames[i] = "worker-" + i;
+            }
+            return workerNames;
+        }
+    
+        private static Callable<Void> createWorker(int treeNumberInclusive, int treeNumberExclusive, AppleTree[] trees, String workerName) {
+            return () -> {
+                for (int i = treeNumberInclusive; i < treeNumberExclusive; i++) {
+                    trees[i].pickApples(workerName);
+                }
+    
+                return null;
+            };
         }
 
-        return trees;
-    }
+        private static class AppleTree {
+            private int id;
+            private int totalApples;
 
-    private static List<Callable<Void>> createWorkers(int numberOfWorkers, AppleTree[] garden) {
-        String[] names = createWorkerNames(numberOfWorkers);
-
-        List<Callable<Void>> workers = new ArrayList<>(numberOfWorkers);
-        workers.add(createWorker(0, 2, garden, names[0]));
-        workers.add(createWorker(2, 4, garden, names[1]));
-        workers.add(createWorker(4, 6, garden, names[2]));
-        
-        return workers;
-    }
-
-    private static String[] createWorkerNames(int numberOfNames) {
-        String[] workerNames = new String[numberOfNames];
-        for (int i = 0; i < numberOfNames; i++) {
-            workerNames[i] = "worker-" + i;
-        }
-        return workerNames;
-    }
-
-    private static Callable<Void> createWorker(int treeNumberInclusive, int treeNumberExclusive, AppleTree[] trees, String workerName) {
-        return () -> {
-            for (int i = treeNumberInclusive; i < treeNumberExclusive; i++) {
-                trees[i].pickApples(workerName);
+            public AppleTree(int id, int totalApples) {
+            this.id = id;
+            this.totalApples = totalApples;
             }
 
-            return null;
-        };
+            public void pickApples(String workerName) {
+            //String picker = Thread.currentThread().getName();
+            String picker = workerName;
+
+            for (int i = 0; i < this.totalApples; i++) {
+                // System.out.println(picker + " is picking apple #" + i + " from tree #" + this.id);
+                try {
+                Thread.sleep(1000); // To simulate the duration of picking apples from 1 tree
+                } catch (InterruptedException ie) {
+                System.out.println(ie.getMessage());
+                }
+            }
+            System.out.println(picker + " finished with tree #" + this.id);
+  }
+        }
     }
+    
 }
